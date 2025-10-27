e5c532dd2d4d590df8ec4c6dd5a7cf79622c2089
Rafael Reis Ramos <rrramos@gmail.com>
2025-10-27 18:33:51 +0000

    feat: add `prettier`, `eslint` and `editorconfig`

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+
+export default defineConfig([
+  {
+    files: ["**/*.js"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+]);
